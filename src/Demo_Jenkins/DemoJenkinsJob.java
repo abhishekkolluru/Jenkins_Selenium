60368242079bf16c60fe9a2b8ae86766e676028a
abhishekkolluru <abhishekkrish9955@gmail.com>
2018-04-02 22:10:33 +0530

    Updated code

--- src/Demo_Jenkins/DemoJenkinsJob.java
+++ src/Demo_Jenkins/DemoJenkinsJob.java
@@ -1,9 +1,15 @@
 package Demo_Jenkins;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class DemoJenkinsJob {
 
@@ -20,9 +26,10 @@ public class DemoJenkinsJob {
 		driver.findElement(By.xpath("//a[contains(text(),'Auto Insurance')]")).click();
 		driver.findElement(By.xpath("//a[contains(text(),'Get a Free')]")).click();
 		System.out.println("Thank you");
-		Thread.sleep(6000);
+		// Thread.sleep(6000);
 		driver.quit();
 	}
+
 	@Test
 	public void Jenkins_1() throws Exception {
 		System.out.println("Test2");
@@ -35,9 +42,24 @@ public class DemoJenkinsJob {
 		driver.findElement(By.xpath("//span[contains(text(),'Products')]")).click();
 		driver.findElement(By.xpath("//a[contains(text(),'Auto Insurance')]")).click();
 		driver.findElement(By.xpath("//a[contains(text(),'Get a Free')]")).click();
-		System.out.println("Thank you...");
+		System.out.println("Thank you");
 		Thread.sleep(5000);
 		driver.quit();
 	}
+
+	public static void captureScreenShot(WebDriver ldriver) {
+		// Take screenshot and store as a file format
+		File src = ((TakesScreenshot) ldriver).getScreenshotAs(OutputType.FILE);
+		try {
+			// now copy the screenshot to desired location using copyFile method
+
+			FileUtils.copyFile(src,
+					new File("C:\\Users\\Abhishek\\Desktop\\Selenium" + System.currentTimeMillis() + ".png"));
+		} catch (IOException e)
+
+		{
+			System.out.println(e.getMessage());
+		}
+
+	}
 }
-	
