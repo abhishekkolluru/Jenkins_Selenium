0a989d1208c25ad8e571473fdde57d7b3d632fe8
abhishekkolluru <abhishekkrish9955@gmail.com>
2018-04-02 22:20:12 +0530

    Removed  the  second @test method

--- src/Demo_Jenkins/DemoJenkinsJob.java
+++ src/Demo_Jenkins/DemoJenkinsJob.java
@@ -22,21 +22,4 @@ public class DemoJenkinsJob {
 		Thread.sleep(6000);
 		driver.quit();
 	}
-
-	@Test
-	public void Jenkins_1() throws Exception {
-		System.out.println("Test2");
-		System.out.println("Hello world");
-		System.out.println("Welcome to the jenkins and selenium integration from github repository");
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.usaa.com");
-		driver.findElement(By.xpath("//span[contains(text(),'Products')]")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'Auto Insurance')]")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'Get a Free')]")).click();
-		System.out.println("Thank you");
-		Thread.sleep(5000);
-		driver.quit();
-	}
 }
