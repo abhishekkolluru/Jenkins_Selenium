cc9c782a29bf50eaf1fdd6aa49bfb1f5c1231adf
abhishekkolluru <abhishekkrish9955@gmail.com>
2018-04-02 22:15:42 +0530

    Updated Code by removing unnecessary files

--- src/Demo_Jenkins/DemoJenkinsJob.java
+++ src/Demo_Jenkins/DemoJenkinsJob.java
@@ -1,21 +1,14 @@
 package Demo_Jenkins;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
 public class DemoJenkinsJob {
 
 	@Test
 	public void Jenkins() throws Exception {
-
 		System.out.println("Hello world");
 		System.out.println("Welcome to the jenkins and selenium integration from github repository");
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
@@ -26,7 +19,7 @@ public class DemoJenkinsJob {
 		driver.findElement(By.xpath("//a[contains(text(),'Auto Insurance')]")).click();
 		driver.findElement(By.xpath("//a[contains(text(),'Get a Free')]")).click();
 		System.out.println("Thank you");
-		// Thread.sleep(6000);
+		Thread.sleep(6000);
 		driver.quit();
 	}
 
@@ -45,21 +38,5 @@ public class DemoJenkinsJob {
 		System.out.println("Thank you");
 		Thread.sleep(5000);
 		driver.quit();
-	}
-
-	public static void captureScreenShot(WebDriver ldriver) {
-		// Take screenshot and store as a file format
-		File src = ((TakesScreenshot) ldriver).getScreenshotAs(OutputType.FILE);
-		try {
-			// now copy the screenshot to desired location using copyFile method
-
-			FileUtils.copyFile(src,
-					new File("C:\\Users\\Abhishek\\Desktop\\Selenium" + System.currentTimeMillis() + ".png"));
-		} catch (IOException e)
-
-		{
-			System.out.println(e.getMessage());
-		}
-
 	}
 }
