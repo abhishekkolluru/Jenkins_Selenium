6d066a2e0d5d00e3ad66e3e63b6284892079d7ca
abhishekkolluru <abhishekkrish9955@gmail.com>
2018-04-02 23:21:38 +0530

    update test file2

--- src/Demo_Jenkins/DemoJenkinsJob2.java
+++ src/Demo_Jenkins/DemoJenkinsJob2.java
@@ -8,13 +8,14 @@ import org.testng.annotations.*;
 public class DemoJenkinsJob2 {
 	
 	@Test
-	public void testJenkins(){
+	public void testJenkins() throws Exception {
 		
 		System.out.println("Welcome to the jenkins");
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http:www.google.com");
-		driver.close();
+		Thread.sleep(6000);
+		driver.quit();
 	}
 
 }
