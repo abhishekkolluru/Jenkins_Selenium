10e655f8579eb59d1be1b926ac157c3fa2f3a2df
abhishekkolluru <abhishekkrish9955@gmail.com>
2018-06-06 07:47:57 +0530

    Added parallel exec and cross browser fun...

--- src/Demo_Jenkins/DemoJenkinsJob.java
+++ src/Demo_Jenkins/DemoJenkinsJob.java
@@ -3,22 +3,84 @@ package Demo_Jenkins;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class DemoJenkinsJob {
 
 	@Test
 	public void Jenkins() throws Exception {
-		System.out.println("Hello world");
+		/*System.out.println("Hello world");
 		System.out.println("Welcome to the jenkins and selenium integration from github repository");
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
+		WebDriver cdriver = new ChromeDriver();
+		cdriver.manage().window().maximize();
+		cdriver.get("https://www.usaa.com");
+		cdriver.findElement(By.xpath("//span[contains(text(),'Products')]")).click();
+		cdriver.findElement(By.xpath("//a[contains(text(),'Auto Insurance')]")).click();
+		cdriver.findElement(By.xpath("//a[contains(text(),'Get a Free')]")).click();
+		System.out.println("Thank you");
+		Thread.sleep(6000);
+		cdriver.quit();*/
+		System.out.println("Welcome to the jenkins");
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Abhishek\\Downloads\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http:www.google.com");
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("jenkins");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@value='Google Search']")).click();
+		Thread.sleep(6000);
+		driver.quit();
+	}
+	@Test
+	public void Jenkins1() throws Exception {
+		/*System.out.println("Hello world");
+		System.out.println("Welcome to the jenkins and selenium integration from github repository");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
+		WebDriver cdriver = new ChromeDriver();
+		cdriver.manage().window().maximize();
+		cdriver.get("https://www.usaa.com");
+		cdriver.findElement(By.xpath("//span[contains(text(),'Products')]")).click();
+		cdriver.findElement(By.xpath("//a[contains(text(),'Auto Insurance')]")).click();
+		cdriver.findElement(By.xpath("//a[contains(text(),'Get a Free')]")).click();
+		System.out.println("Thank you");
+		Thread.sleep(6000);
+		cdriver.quit();*/
+		System.out.println("Welcome to the jenkins");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.usaa.com");
-		driver.findElement(By.xpath("//span[contains(text(),'Products')]")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'Auto Insurance')]")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'Get a Free')]")).click();
+		driver.get("http:www.google.com");
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("jenkins");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@value='Google Search']")).click();
+		Thread.sleep(6000);
+		driver.quit();
+	}
+	@Test
+	public void JenkinsEdge() throws Exception {
+		/*System.out.println("Hello world");
+		System.out.println("Welcome to the jenkins and selenium integration from github repository");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
+		WebDriver cdriver = new ChromeDriver();
+		cdriver.manage().window().maximize();
+		cdriver.get("https://www.usaa.com");
+		cdriver.findElement(By.xpath("//span[contains(text(),'Products')]")).click();
+		cdriver.findElement(By.xpath("//a[contains(text(),'Auto Insurance')]")).click();
+		cdriver.findElement(By.xpath("//a[contains(text(),'Get a Free')]")).click();
 		System.out.println("Thank you");
+		Thread.sleep(6000);
+		cdriver.quit();*/
+		System.out.println("Welcome to the jenkins");
+		System.setProperty("webdriver.edge.driver","C:\\Users\\Abhishek\\Downloads\\MicrosoftWebDriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("http:www.google.com");
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("jenkins");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@value='Google Search']")).click();
 		Thread.sleep(6000);
 		driver.quit();
 	}
