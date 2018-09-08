1708dcdb13e54abc06e54f22d138fc9782175fed
abhishekkolluru <abhishekkrish9955@gmail.com>
2018-09-08 22:41:34 +0530

    updated till date 8/9/18

--- src/Demo_Jenkins/DemoJenkinsJob2.java
+++ src/Demo_Jenkins/DemoJenkinsJob2.java
@@ -1,20 +1,40 @@
 package Demo_Jenkins;
 
+import java.io.File;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class DemoJenkinsJob2 {
-	
+	ExtentReports extent;
+	ExtentTest logger;
+
+	@BeforeTest
+	public void startReport() {
+		extent = new ExtentReports(System.getProperty("user.dir") + "/test-output/STMExtentReport.html", true);
+		extent.addSystemInfo("Host Name", "Testing").addSystemInfo("Environment", "Automation Testing")
+				.addSystemInfo("User Name", "Abhishek");
+		extent.loadConfig(new File(System.getProperty("user.dir") + "\\extent-config.xml"));
+	}
+
 	@Test
 	public void testJenkins() throws Exception {
-		
 		System.out.println("Welcome to the jenkins");
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http:www.google.com");
@@ -22,22 +42,14 @@ public class DemoJenkinsJob2 {
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//input[@value='Google Search']")).click();
 		Thread.sleep(6000);
+		logger = extent.startTest("testJenkins");
+		Assert.assertTrue(true);
+		logger.log(LogStatus.PASS, "Test Case Passed is testJenkins");
 		driver.quit();
 	}
+	
 	@Test
-	public void Jenkins() throws Exception {
-		/*System.out.println("Hello world");
-		System.out.println("Welcome to the jenkins and selenium integration from github repository");
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
-		WebDriver cdriver = new ChromeDriver();
-		cdriver.manage().window().maximize();
-		cdriver.get("https://www.usaa.com");
-		cdriver.findElement(By.xpath("//span[contains(text(),'Products')]")).click();
-		cdriver.findElement(By.xpath("//a[contains(text(),'Auto Insurance')]")).click();
-		cdriver.findElement(By.xpath("//a[contains(text(),'Get a Free')]")).click();
-		System.out.println("Thank you");
-		Thread.sleep(6000);
-		cdriver.quit();*/
+	public void testJenkins2() throws Exception {
 		System.out.println("Welcome to the jenkins");
 		System.setProperty("webdriver.gecko.driver","C:\\Users\\Abhishek\\Downloads\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
@@ -47,32 +59,27 @@ public class DemoJenkinsJob2 {
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//input[@value='Google Search']")).click();
 		Thread.sleep(6000);
+		logger = extent.startTest("testJenkins2");
+		Assert.assertTrue(true);
+		logger.log(LogStatus.PASS, "Test Case Passed is testJenkins2");
 		driver.quit();
 	}
-	@Test
-	public void JenkinsEdge() throws Exception {
-		/*System.out.println("Hello world");
-		System.out.println("Welcome to the jenkins and selenium integration from github repository");
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Abhishek\\Downloads\\chromedriver.exe");
-		WebDriver cdriver = new ChromeDriver();
-		cdriver.manage().window().maximize();
-		cdriver.get("https://www.usaa.com");
-		cdriver.findElement(By.xpath("//span[contains(text(),'Products')]")).click();
-		cdriver.findElement(By.xpath("//a[contains(text(),'Auto Insurance')]")).click();
-		cdriver.findElement(By.xpath("//a[contains(text(),'Get a Free')]")).click();
-		System.out.println("Thank you");
-		Thread.sleep(6000);
-		cdriver.quit();*/
-		System.out.println("Welcome to the jenkins");
-		System.setProperty("webdriver.edge.driver","C:\\Users\\Abhishek\\Downloads\\MicrosoftWebDriver.exe");
-		WebDriver driver = new EdgeDriver();
-		driver.manage().window().maximize();
-		driver.get("http:www.google.com");
-		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("jenkins");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[@value='Google Search']")).click();
-		Thread.sleep(6000);
-		driver.quit();
+
+	@AfterMethod
+	public void getResult(ITestResult result) {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			logger.log(LogStatus.FAIL, "Test Case Failed is " + result.getName());
+			logger.log(LogStatus.FAIL, "Test Case Failed is " + result.getThrowable());
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			logger.log(LogStatus.SKIP, "Test Case Skipped is " + result.getName());
+		}
+		extent.endTest(logger);
+	}
+
+	@AfterTest
+	public void endReport() {
+		extent.flush();
+		extent.close();
 	}
 
 }
